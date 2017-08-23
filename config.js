771a42557040ef444ffe41fe577bd5c7b1260462
Tyler Speaks <speaks_tyler@yahoo.com>
2017-08-22 23:45:16 -0500

    updated config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+                      'mongodb://localhost/mongoose-blog-app';
 exports.PORT = process.env.PORT || 8080;
